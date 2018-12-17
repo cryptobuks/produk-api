20c617a7749059bc8ebc7dc3b4ed225d2007d91d
Devvara Rishivian <rishivian@gmail.com>
2018-12-17 11:29:41 +0700

    Using dotenv module to hide sensitive credential

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,15 @@
-import mongodbcredentials from './mongodbcredentials';
 import { Module, HttpModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 
+require('dotenv').config();
 @Module({
   imports: [
     HttpModule,
     ProductsModule,
-    // MongooseModule.forRoot('mongodb://localhost:27017/produk'),
-    MongooseModule.forRoot(mongodbcredentials),
+    MongooseModule.forRoot(process.env.DB_URL),
   ],
   controllers: [AppController],
   providers: [AppService],
